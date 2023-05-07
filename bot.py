f7f11f14fee8587e52a5f919bc191bea7573fc83
A_V <82163471+Anton-vok@users.noreply.github.com>
2023-05-08 00:05:21 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,5 +2,5 @@ import telebot
 bot=telebot.TeleBot("6024635066:AAFGjWIB62DdBx355aCCduZJdTKvBphnsBo")
 @bot.message_handler(commands=['start'])
 def echo(message):
-  bot.send_message(message,chat.id,message.text)
+  bot.send_message(message.chat.id,message.text)
 bot.polling(none_stop=True)
