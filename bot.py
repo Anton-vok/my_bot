24a3f091a0e70a65783700d2d40f44e91421cb36
A_V <82163471+Anton-vok@users.noreply.github.com>
2023-05-08 00:00:00 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,9 @@
 import telebot
 bot=telebot.TeleBot("6024635066:AAFGjWIB62DdBx355aCCduZJdTKvBphnsBo")
-@bot.message_handler(content_types=['text'])
+@bot.message_handler(commands=['start'])
 def echo(message):
-  bot.send_message(message.chat.id,message.text)
+  bot.send_message(message.chat.id,"Привет")
+@bot.message_hendler(commands=["new"])
+def new(message):
+  bot.send_message(message.chat.id,"ты ввел /new")
 bot.polling(none_stop=True)
