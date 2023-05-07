01221b4fff0146031879ef9e477b64e65f22ca07
A_V <82163471+Anton-vok@users.noreply.github.com>
2023-05-07 23:49:11 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,4 +3,4 @@ bot=telebot.TeleBot("6024635066:AAFGjWIB62DdBx355aCCduZJdTKvBphnsBo")
 @bot.message_handler(content_types=['text'])
 def echo(message):
   bot.send_message(message,chat.id,message.text)
-bot.polling(none_stop=true)
+bot.polling(none_stop=True)
