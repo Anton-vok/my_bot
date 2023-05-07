06ee3199f36ad64f81c5bca0c8c60bf405f97b94
A_V <82163471+Anton-vok@users.noreply.github.com>
2023-05-08 00:43:15 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,5 +2,9 @@ import telebot
 bot=telebot.TeleBot("6024635066:AAFGjWIB62DdBx355aCCduZJdTKvBphnsBo")
 @bot.message_handler(commands=['start'])
 def echo(message):
-  bot.send_message(message.chat.id,message.text)
+    bot.send_message(message.chat.id,'Привет. Я создан для раздачи ролей в ролевой игре "The Adventurers Guild".')
+    bot.send_message(message.chat.id,"https://t.me/GenRolACHV")
+    bot.send_message(message.chat.id,"Если ты сейчас напишешь /new, бот отправит тебе твою роль.")
+    bot.send_message(message.chat.id,"Если что-то не работает, сообщи мне: @A_CH_V")
+    bot.send_message(message.chat.id,"(пожалуйста, не ломайте ничего, он и так сделан на коленке)")
 bot.polling(none_stop=True)
