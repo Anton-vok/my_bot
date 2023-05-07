d17573da6ab11de93e01f5d47822cc3c2fb01c25
A_V <82163471+Anton-vok@users.noreply.github.com>
2023-05-07 21:51:39 +0400

    Update uwu.py

--- uwu.py
+++ uwu.py
@@ -2,13 +2,10 @@ import telegram
 
 bot = telegram.Bot(token="6024635066:AAFGjWIB62DdBx355aCCduZJdTKvBphnsBo")
 
-def start_command(update, context):
-    update.message.reply_text('Привет. Я создан для раздачи ролей в ролевой игре "The Adventurers Guild".')
-    update.message.reply_text("https://t.me/GenRolACHV")
-    update.message.reply_text("Если ты сейчас напишешь /new, бот отправит тебе твою роль.")
-    update.message.reply_text("Если что-то не работает, сообщи мне: @A_CH_V")
-    update.message.reply_text("(пожалуйста, не ломайте ничего, он и так сделан на коленке)")
-
-bot.add_command("start", start_command)
-
-bot.start()
+@bot.message_handler(commands=["start"])
+def start(message):
+    bot.send_message(message.chat.id,"Привет. Я создан для раздачи ролей в ролевой игре 'The Adventurers Guild'.")
+    bot.send_message(message.chat.id,"https://t.me/GenRolACHV")
+    bot.send_message(message.chat.id,"Если ты сейчас напишешь /new, бот отправит тебе твою роль.")
+    bot.send_message(message.chat.id,"Если что-то не работает, сообщи мне: @A_CH_V")
+    bot.send_message(message.chat.id,"(пожалуйста, не ломайте ничего, он и так сделан на коленке)")
