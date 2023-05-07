19702fa01e98ef655032c66ee1a3a5dccdad6cb6
A_V <82163471+Anton-vok@users.noreply.github.com>
2023-05-08 01:00:19 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,34 +1,49 @@
 import telebot
 bot=telebot.TeleBot("6024635066:AAFGjWIB62DdBx355aCCduZJdTKvBphnsBo")
-history=[]
-min_players=1
-roles=[
-    ["one",None,None,1],
-    ["two",None,None,1]
+import random
+
+history = []
+min_players = 1
+roles = [
+    ["one", None, None, 1],
+    ["two", None, None, 1]
 ]
-history_user=[]
+history_user = []
+
 def assign_role(history, min_players, roles):
-    roles_with_min_requirement = [role for role in roles if history.count(role[0]) < role[1]]
+    # Список ролей, которые еще не набраны минимальное количество раз.
+    roles_with_min_requirement = [role for role in roles if role[1] is not None and history.count(role[0]) < role[1]]
 
     if len(roles_with_min_requirement) > 0:
+        # Если есть доступные роли, которые нужно набрать минимальное количество раз, 
+        # выбираем случайную роль из этого списка и возвращаем ее имя.
         return random.choice(roles_with_min_requirement)[0]
 
-    remaining_slots = min_players - len(history)
-    remaining_roles = []
+    # Если ни одна из ролей не требует дополнительных игроков, то пробуем назначить роли по умолчанию.
+    remaining_slots = max(min_players - len(history), 0) # Оставшееся количество игроков, которые нужно распределить.
 
-    for role in roles:
-        if role[2] is None or history.count(role[0]) < role[2]:
-            remaining_roles.append(role)
-        else:
-            remaining_slots += role[1]
+    # Список ролей, которые еще не набраны максимальное количество раз.
+    remaining_roles = [role for role in roles if role[2] is None or history.count(role[0]) < role[2]]
 
+    # Список ролей, из которых будут выбираться роли для назначения.
     role_chances = []
 
+    # Если осталось меньше ролей, чем игроков, то мы назначаем все доступные роли и возвращаем их.
+    if remaining_slots <= len(remaining_roles):
+        for role in remaining_roles[:remaining_slots]:
+            history.append(role[0])
+        return history
+
+    # Если осталось больше ролей, чем игроков, то мы выбираем роли с вероятностями,
+    # пропорциональными их "весам" (третий параметр в списке roles).
     for role in remaining_roles:
         role_count = remaining_slots * role[3] // sum(r[3] for r in remaining_roles)
         role_chances += [role[0]] * role_count
 
+    # Выбираем случайную роль из списка возможных ролей и возвращаем ее имя.
     return random.choice(role_chances)
+
+
 
 
 
