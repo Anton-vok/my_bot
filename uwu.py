970acbb42410305d1be2acdb101accafe5cf3d3d
A_V <82163471+Anton-vok@users.noreply.github.com>
2023-05-07 22:06:42 +0400

    Update uwu.py

--- uwu.py
+++ uwu.py
@@ -5,3 +5,5 @@ dp = Dispatcher(bot)
 @dp.message_handler(commands=['start']) #Явно указываем в декораторе, на какую команду реагируем. 
 async def send_welcome(message: types.Message):
    await message.reply("Привет!\nЯ Эхо-бот от Skillbox!\nОтправь мне любое сообщение, а я тебе обязательно отвечу.") #Так как код работает асинхронно, то обязательно пишем await.
+if __name__ == '__main__':
+   executor.start_polling(dp, skip_updates=True)
