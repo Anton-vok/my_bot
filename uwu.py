63b72956fe03458ed358167d9fd8e177e6dcc1e3
A_V <82163471+Anton-vok@users.noreply.github.com>
2023-05-09 02:50:36 +0400

    Update uwu.py

--- uwu.py
+++ uwu.py
@@ -1,24 +1,9 @@
-import telegram
-from telegram.ext import Updater, CommandHandler
+import telebot
+bot=telebot.TeleBot("6024635066:AAFGjWIB62DdBx355aCCduZJdTKvBphnsBo")
+import random
+from collections import Counter
 
-def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Привет!")
-
-def main():
-    # Создаем объект Updater и передаем ему токен бота
-    updater = Updater("6024635066:AAFGjWIB62DdBx355aCCduZJdTKvBphnsBo")
-
-    # Получаем объект диспетчера для регистрации обработчиков
-    dp = updater.dispatcher
-
-    # Регистрируем обработчик команды /start
-    dp.add_handler(CommandHandler("start", start))
-
-    # Запускаем бота
-    updater.start_polling()
-
-    # Останавливаем бота при получении сигнала SIGINT (Ctrl+C в терминале)
-    updater.idle()
-
-if __name__ == '__main__':
-    main()
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id,message.from_chat.id)
+bot.polling(none_stop=True)    
